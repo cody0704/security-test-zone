0e5df74b01b31adf29fcd26e7e6fee4a7f727bff
Cody Chen <57340378+cody0704@users.noreply.github.com>
2020-07-21 09:45:22 +0800

    Update lodash.js

--- lodash.js
+++ lodash.js
@@ -1,11 +1,3 @@
-/**
- * @license
- * Lodash <https://lodash.com/>
- * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
- * Released under MIT license <https://lodash.com/license>
- * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
- * Copyright Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
- */
 ;(function() {
 
   /** Used as a safe reference for `undefined` in pre-ES5 environments. */
